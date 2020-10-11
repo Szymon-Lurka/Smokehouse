8d24234e746a5f38bafe9cb0666124fb72249cae
Szymon Lurka <szymon.lurka@interia.pl>
2020-10-11 20:53:17 +0200

    change structure of site

--- js/init.js
+++ js/init.js
@@ -1,7 +1,12 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
+$(document).ready(function () {
+  $('.carousel').carousel();
+  $('.collapsible').collapsible();
+  $('.modal').modal();
+});
