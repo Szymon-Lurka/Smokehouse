415efbb80400ef303a36c3867e8e6dd247013c80
Szymon Lurka <szymon.lurka@interia.pl>
2020-10-12 00:37:14 +0200

    add preloader

--- js/init.js
+++ js/init.js
@@ -9,4 +9,8 @@ $(document).ready(function () {
   $('.carousel').carousel();
   $('.collapsible').collapsible();
   $('.modal').modal();
+  $('.preloader-background').delay(1700).fadeOut('slow');
+  $('.preloader-wrapper')
+    .delay(1700)
+    .fadeOut();
 });
